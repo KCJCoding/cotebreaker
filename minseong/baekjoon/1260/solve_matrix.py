c996727be1edcf697cc47265e23e8d51c4572a3e
kordood <gigacms@gmail.com>
2022-03-26 16:42:21 +0900

    Progress: Inaccurate bfs

--- minseong/baekjoon/1260/solve_matrix.py
+++ minseong/baekjoon/1260/solve_matrix.py
@@ -6,23 +6,43 @@ def solve():
         u, v = map(int, input().split())
         graph[u][v] = graph[v][u] = 1
 
-    dfs(graph, [V])
-    #bfs(graph, V)
+    print_list(dfs(graph, [V]))
+    print_list(bfs(graph, [V]))
 
 
 def dfs(graph, discovered=None):
     v = discovered[-1]
-    print(v)
 
     for i, node in enumerate(graph[v]):
         if node == 1 and i not in discovered:
             discovered.append(i)
-            dfs(graph, discovered)
+            discovered = dfs(graph, discovered)
+
+    return discovered
 
 
-def bfs(graph, V):
-    pass
+def bfs(graph, discovered=None):
+    while True:
+        if len(graph) <= 0:
+            break
+
+        sub_graph = graph[0]
+        graph = graph[1:]
+        for i, edge in enumerate(sub_graph):
+            if edge == 1:
+                sub_graph[i] = 0
+
+                if i not in discovered:
+                    discovered.append(i)
+
+    return discovered
+
+
+def print_list(list):
+    for element in list:
+        print(element, end=" ")
+    print("", end="\n")
 
 
 if __name__ == '__main__':
-    print(solve())
+    solve()
