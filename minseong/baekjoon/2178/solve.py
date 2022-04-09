1eec88f205e329157928962d642f319501f0bf7e
kordood <gigacms@gmail.com>
2022-04-09 14:33:09 +0900

    Refactor: Remove unused codes such as snippet of trying dfs

--- minseong/baekjoon/2178/solve.py
+++ minseong/baekjoon/2178/solve.py
@@ -1,9 +1,7 @@
 def solve():
     N, M = map(int, input().split(' '))
     graph = make_graph(N)
-    traverse = bfs
-    #dfs(graph, [(0, 0)], N-1, M-1)
-    graph = traverse(graph, N-1, M-1)
+    graph = bfs(graph, N-1, M-1)
     print(graph[N-1][M-1])
 
 
@@ -14,27 +12,6 @@ def make_graph(N):
         graph.append([int(node) for node in input()])
 
     return graph
-
-
-def dfs(graph, discovered, N, M):
-    xy_list = [(1, 0), (0, 1), (0, -1), (-1, 0)]
-    i, j = discovered[-1]
-    graph[i][j] = 0
-
-    if i == N and j == M:
-        #print(discovered)
-        print(len(discovered))
-
-    for x, y in xy_list:
-        index_x = i + x
-        index_y = j + y
-
-        if 0 <= index_x <= N and 0 <= index_y <= M:
-            if graph[index_x][index_y] == 1:
-                discovered.append((index_x, index_y))
-                discovered = dfs(graph, discovered, N, M)
-
-    return discovered
 
 
 def bfs(graph, N, M):
