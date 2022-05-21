651129fb04f82fe8b8cd1e80b524e7281271f10c
eunjin115 <katherine0115@naver.com>
2022-05-21 23:03:56 +0900

    Solved: [book] 효율적인화폐구성

---
+++ eunjin/Problem/Dynamic Programming/효율적인화폐구성.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <algorithm>
+
+int N, M;
+std::vector<int> coin;
+int main()
+{
+	int tmp;
+
+	std::cin >> N >> M; 
+	for (int i = 0; i < tmp; i++)
+	{
+		std::cin >> tmp;
+		coin.push_back(tmp);
+	}
+	std::vector<int> d(M + 1, std::numeric_limits<int>::max());
+	d[0] = 0;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = coin[i]; j <= M; j++)
+		{
+			if (d[j - coin[i]] != std::numeric_limits<int>::max())
+				d[j] = std::min(d[j], d[j - coin[i]] + 1);
+		}
+	}
+	std::cout << d[M];
+	return 0;
+}
