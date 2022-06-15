76fce8eb51c0b16e82a9acd93d2c53b548ecbc7e
eunjin115 <katherine0115@naver.com>
2022-06-15 12:39:20 +0900

    Solved : BOJ 1191

--- eunjin/Problem/탐색/queue.cpp
+++ eunjin/Problem/탐색/queue.cpp
@@ -3,6 +3,21 @@
 
 int main()
 {
+	std::queue<int> q;
+	q.push(5);
+	q.push(3);
+	q.push(7);
+
+	q.pop();
+	q.push(1);
+	q.push(4);
+	q.pop();
+
+	while (!q.empty())
+	{
+		std::cout << q.front() << '\n';
+		q.pop();
+	}
 
 	return 0;
 }
