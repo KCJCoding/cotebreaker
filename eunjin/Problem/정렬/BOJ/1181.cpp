e40059ab0ad99cb9a6d8091e45d8696d9e595b51
Eunjin <katherine0115@naver.com>
2022-06-17 15:35:20 +0900

    Solved : BOJ 1181 (#130)
    * Solved: BOJ 1654
    
    * Solved: BOJ 10816
    
    * Solved: [book] Dynamic Programming example
    
    * Solved: [book] 1로 만들기
    
    * Solved: [book] 개미전사
    
    * Solved: [book] 바닥공사
    
    * Solved: BOJ 1003
    
    * Solved: [book] 효율적인화폐구성
    
    * Solved: [book] 효율적인화폐구성 - 수정
    
    * Solved : PGS 가운데 글자 가져오기
    
    * Update: BOJ Simulation Problems
    
    * Solved: BOJ 2581
    
    * Chore: 파일 구조 변경
    
    * Chore: 폴더이름변경
    
    * Solved: PGS 기능개발
    
    * Solved : PGS 프린터
    
    * Solved : BOJ 2309
    
    * Solved : BOJ 1191
    
    * Solved : BOJ 1012
    
    * Solved : PGS 핸드폰번호가리기
    
    * Solved : PGS K번째 수
    
    * Solved : PGS 모의고사
    
    * Solved : PGS 더 맵게
    
    * Solved : BOJ_연구소
    
    * Solved : BOJ 1181

--- eunjin/Problem/정렬/BOJ/1181.cpp
+++ eunjin/Problem/정렬/BOJ/1181.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+bool compare(const std::string &a, const std::string &b)
+{
+	if (a.length() == b.length()) //길이가 같은 경우에는 
+		return a < b;			  //알파벳 순으로 정렬 
+	return a.length() < b.length(); //기본적으로는 길이로 정렬 
+}
 
 int main()
 {
+	int N;
+	std::string tmp;
+	std::vector<std::string> str;
+	
+	std::cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		std::cin >> tmp;
+		str.push_back(tmp);
+	}
+
+	std::sort(str.begin(), str.end());
+	str.erase(std::unique(str.begin(), str.end()), str.end());
+	std::sort(str.begin(), str.end(), compare);
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		std::cout << str[i] << '\n';
+	}
 
 	return 0;
 }
