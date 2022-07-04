af006d40277771261e15b980fdff91abb90fc89b
eunjin115 <katherine0115@naver.com>
2022-07-05 00:57:48 +0900

    Solved: BOJ 9663

---
+++ eunjin/Problem/완전탐색/BOJ/9663.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+
+#define MAX 15
+
+using namespace std;
+
+int queen[MAX];
+int N, answer = 0;
+
+bool check(int x)
+{
+	for (int i = 0; i < x; i++)
+	{
+		if (queen[x] == queen[i] || x - i == abs(queen[x] - queen[i])) //같은 열이나 대각선에 있는 경우
+			return false; 
+	}
+	return true;
+}
+
+void nqueen(int x)
+{
+	if (x == N)
+	{
+		answer++;
+		return;
+	}
+	else
+	{
+		for (int i = 0; i < N; i++)
+		{
+			queen[x] = i; //현재 행 배열 위치에 i (열) 놓기 -> 퀸 놓기
+			if (check(x)) //현재 행 열 위치가 괜찮다면 다음 행에 퀸 놓기 
+			{
+				nqueen(x + 1);
+			}
+		}
+	}
+
+}
+
+int main()
+{
+	cin >> N;
+	nqueen(0);
+	cout << answer;
+	return 0;
+}
